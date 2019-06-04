17375a231310f99b9cf7b550d385b68a4b2dd88c
faithngotho <faithwangari248@gmail.com>
2019-06-05 00:34:02 +0300

    add added jumbotron on quote.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Quote } from './quote'
 
 @Component({
-  selector: 'app-root'
+  selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
