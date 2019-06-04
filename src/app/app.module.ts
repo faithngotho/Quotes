974f90f4d1c28aeb89313fad084f468654c87a95
faithngotho <faithwangari248@gmail.com>
2019-06-04 08:27:20 +0300

    add added file to the quote component html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent }   from './quote-details/quote-details.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component'
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    QuoteFormComponent
+    
   ],
   imports: [
     BrowserModule
