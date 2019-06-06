ed2727a7bb6bd7500ed7cb9b786ffc43e858679a
faithngotho <faithwangari248@gmail.com>
2019-06-06 07:56:05 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,7 +0,0 @@
-export class Quote {
-    public showDescription:boolean
-    constructor(public author:string, public quote:string, public name:string, public upvote:number, public downvote:number, public postDate:Date){
-        this.showDescription=false
-
-    }
-}
